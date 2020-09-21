7a2eb9e86b7e085fffcdd9fe38f8c150059d260f
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-09-21 14:28:27 +0700

    Add parameter to jdk command

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,5 +1,8 @@
 jdk() {
-        version=$1
-        export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
-        java -version
- }
+  version=$1
+  export java_home=$(/usr/libexec/java_home -v"$version");
+  if [ -z "$2" ]; then
+    java -version
+  fi
+}
+
