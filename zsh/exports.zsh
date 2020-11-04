15fd7699d4317c8c13a645d1f13cb8a8d51fcb11
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-11-04 18:24:51 +0700

    Remove device specific configs

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,26 +4,8 @@ export EDITOR='nvim'
 # GO
 export GOPATH=$HOME/Go
 
-# Java
-export JUNIT_HOME=/Library/JUNIT
-export JDEPEND_HOME=/Library/jdepend-2.9.1
-export CLASSPATH=$CLASSPATH:$JUNIT_HOME/junit-4.10.jar:.
-export CLASSPATH=$CLASSPATH:$JDEPEND_HOME/lib/jdepend-2.9.1.jar
-
-# Maven
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.5.3
-export M2=$M2_HOME/bin
-export MAVEN_OPTS=-Xms256m #-Xmx512m
-
-export PGDATABASE=postgres
-
 # Kubernetes Editor
 export KUBE_EDITOR="nvim"
-
-# Sqlite
-export LDFLAGS="-L/usr/local/opt/sqlite/lib"
-export CPPFLAGS="-I/usr/local/opt/sqlite/include"
-export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
 
 # Locale
 export LANG=en_US.UTF-8
