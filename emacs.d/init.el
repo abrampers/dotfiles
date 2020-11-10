d01c2092ef1b225093a97deb4e41b72266b55bbf
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-11-10 10:12:15 +0700

    Remove treemacs section in keybindings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -617,5 +617,3 @@
             (evil-local-set-key 'normal (kbd ",a") 'projectile-toggle-between-implementation-and-test)
             (evil-ex-define-cmd "A" 'projectile-toggle-between-implementation-and-test)
             (evil-ex-define-cmd "AV" 'projectile-find-implementation-or-test-other-window)))
-
-
