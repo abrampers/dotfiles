407ed77798357b4826a6d79a10fc995330968119
Abram Situmorang <abram.perdanaputra@gmail.com>
2020-11-16 18:12:44 +0700

    Add ox-hugo

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -567,6 +567,32 @@
 
 (use-package yaml-mode)
 
+(use-package ox-hugo
+  :after ox)
+
+(with-eval-after-load 'org-capture
+    (defun abram/org-hugo-new-subtree-post-capture-template ()
+      "Returns `org-capture' template string for new Hugo post.
+See `org-capture-templates' for more information."
+      (let* ((title (read-from-minibuffer "Post Title: "))
+             (fname (org-hugo-slug title)))
+        (mapconcat #'identity
+                   `(
+                     ,(concat "* TODO " title)
+                     ":PROPERTIES:"
+                     ,(concat ":EXPORT_FILE_NAME: " fname)
+                     ":END:"
+                     "#+toc: headlines 1 local"
+                     "%?\n")
+                   "\n")))
+
+    (add-to-list 'org-capture-templates
+                 '("b"
+                   "Hugo blogpost"
+                   entry
+                   (file+olp "BlogContent.org" "blog")
+                   (function abram/org-hugo-new-subtree-post-capture-template))))
+
 (use-package general
   :config
   (general-create-definer abram/leader-keys-map
