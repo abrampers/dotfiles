cf27442bc94aaa7bdf1bafd011347dc02f01aec2
Abram Situmorang <abram.perdanaputra@gmail.com>
2021-02-16 21:58:09 +0700

    Defer magit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -674,6 +674,7 @@
   (company-idle-delay 0.0))
 
 (use-package magit
+  :commands magit-status
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
   :init
