0e93900f3b032e5154a73bf3d89dd0033c3730f3
Abram Situmorang <abram.perdanaputra@gmail.com>
2021-08-05 17:49:52 +0700

    Add sample private init

---
+++ emacs.d/sample.private-init.el
@@ -0,0 +1,8 @@
+(defvar abram/blog-content-org-file)
+(setq abram/blog-content-org-file "~/Code/projects/blog.abram.id/content.org")
+
+(defvar abram/org-directory)
+(setq abram/org-directory "~/org")
+
+(defvar abram/org-roam-directory)
+(setq abram/org-roam-directory "~/roam")
